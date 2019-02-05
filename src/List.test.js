da855f1e926fa8eb119cc9ceb0528afdb78dacd7
JuiceboxJones <jderhalli@gmail.com>
2019-02-05 08:34:00 -0800

    testing list

--- src/List.test.js
+++ src/List.test.js
@@ -1,3 +1,20 @@
 import React from 'react';
-import Card from './Card';
 import List from './List';
+import ReactDOM from 'react-dom';
+
+describe('<List />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List  />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
+
+// describe('<Card />', () => {
+//   it('renders the UI as expected', () => {
+//     const tree = renderer
+//       .create(<Card title = "first card" content = "lorem ipsum"/>)
+//       .toJSON();
+//     expect(tree).toMatchSnapshot();  
+//     });
+// })
